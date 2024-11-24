cab8a1efbe48fbacf068f9b28c1c3269f0df9dcf
Chris <Chris08tm@hotmail.com>
2024-11-23 20:51:56 -0600

    fixed footer buttons

--- src/components/BPIFooter.js
+++ src/components/BPIFooter.js
@@ -4,8 +4,7 @@ export default function BPIFooter() {
       <div class="footer-content">
         <p>&copy; 2024 Byte Precision IT. All rights reserved.</p>
         <nav class="footer-links">
-          <a href="#about">About Us</a>
-          <a href="#services">Services</a>
+          <a href="#services-page">Services</a>
           <a href="#contact">Contact</a>
           <a href="#privacy">Privacy Policy</a>
         </nav>
